58c9c3d154a0411a0b900b39a91eb277ab9f6963
Pouria Jahandideh <pjsofts@gmail.com>
2023-08-10 15:11:14 +0330

    added basic info

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Lessons({ sections }) {
         </div>
         {courseInfo.frontendMastersLink ? (
           <a href={courseInfo.frontendMastersLink} className="cta-btn">
-            Watch on Frontend Masters
+            Watch on Amanj Academy
           </a>
         ) : null}
         <div className="main-card">
