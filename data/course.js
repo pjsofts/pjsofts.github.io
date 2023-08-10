0f275f7d5247fb029b4c82d225502b7745e04d4d
Pouria Jahandideh <pjsofts@gmail.com>
2023-08-10 15:56:01 +0330

    changed token

--- data/course.js
+++ data/course.js
@@ -15,7 +15,7 @@ const DEFAULT_CONFIG = {
     github: "btholt",
     twitter: "holtbt",
   },
-  productionBaseUrl: "/",
+  productionBaseUrl: "/pjsofts.github.io",
 };
 
 export default function getCourseConfig() {
